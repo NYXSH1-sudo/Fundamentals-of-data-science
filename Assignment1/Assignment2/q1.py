3ca2d24223d56243328d9c9e8c687212161d49eb
seshank kumar sharma <sheshanksharma61@gmail.com>
2026-04-16 13:42:25 +0545

    q1

---
+++ Assignment1/Assignment2/q1.py
@@ -0,0 +1,15 @@
+def calculate(a, b):
+    print("Sum:", a + b)
+    print("Difference:", a - b)
+    print("Product:", a * b)
+    
+    if b != 0:
+        print("Quotient:", a / b)
+    else:
+        print("Quotient: Cannot divide by zero")
+
+
+num1 = int(input("Enter first integer: "))
+num2 = int(input("Enter second integer: "))
+
+calculate(num1, num2)
