31a88a375a0a6ed604d66339fcd5d450d26dfd80
seshank kumar sharma <sheshanksharma61@gmail.com>
2026-04-16 14:26:36 +0545

    q7

---
+++ Assignment1/Assignment2/q7.py
@@ -0,0 +1,14 @@
+def key_value(sentence):
+    freq = {}
+    
+    for char in sentence:
+        if char != " ":   # exclude spaces
+            freq[char] = freq.get(char, 0) + 1
+    
+    return freq
+
+
+sentence = input("Enter a sentence: ")
+
+result = key_value(sentence)
+print("Character frequency:", result)
