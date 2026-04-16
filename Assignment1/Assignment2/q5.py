8a1cfa9dc8aafaca424ba233453e381c0c9f433e
seshank kumar sharma <sheshanksharma61@gmail.com>
2026-04-16 14:21:51 +0545

    q5

---
+++ Assignment1/Assignment2/q5.py
@@ -0,0 +1,11 @@
+def sort_names_reverse(names):
+    return sorted(names, reverse=True)
+
+
+students1 = ["bikkey", "NYX", "Bikash", "Rishi"]
+students2 = ["Spandan", "subigya", "siddhant", "Bimal"]
+students3 = ["raunak", "saksham", "sushant", "prakash"]
+
+print(sort_names_reverse(students1))
+print(sort_names_reverse(students2))
+print(sort_names_reverse(students3))
